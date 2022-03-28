28f5207188fb7360f20bb78dfe6251dfae962b18
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-03-28 19:45:46 +0530

    login and signup updated

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,27 @@ const express = require('express');
 const { chats } = require('./data/data');
 const dotenv = require('dotenv');
 const connectDB = require('./Config/db');
+const userRoutes = require('./routes/userRoutes');
+
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 dotenv.config();
 connectDB();
 const app = express();
-
+app.use(express.json()); //telling server to accept json data
 app.get('/', (req, res) => {
     res.send("ApI is running");
 })
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-})
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
-
+// app.get('/api/chat', (req, res) => {
+//     res.send(chats);
+// })
+// app.get('/api/chat/:id', (req, res) => {
+//     const singleChat = chats.find((c) => c._id === req.params.id);
+//     res.send(singleChat);
+// })
+app.use('/api/user', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Running Successfully on ${PORT}`));
--- frontend/src/Components/Authentication/Signup.js
+++ frontend/src/Components/Authentication/Signup.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import {Button,FormControl,FormLabel,Input,InputGroup,InputRightElement,VStack } from '@chakra-ui/react'
+import { useToast } from '@chakra-ui/react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const Signup = () => {
   const [show1,setShow1]=useState(false);
   const [show2,setShow2]=useState(false);
@@ -8,7 +11,9 @@ const Signup = () => {
   const [password,setPassword]=useState();
   const [confirmpassword,setConfirmpassword]=useState();
   const [pic,setPic]=useState();
-
+  const[loading,setLoading]=useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
   const handleClick1=()=>{
       setShow1(!show1);
   }
@@ -16,10 +21,100 @@ const Signup = () => {
       setShow2(!show2);
   }
   const postDetails=(pics)=>{
+    //https://api.cloudinary.com/v1_1/leetcode/    base api
+    setLoading(true);
+    if(pics===undefined){
+      toast({
+        title: 'Please Select an Image!',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position:"bottom",
+      });  
+      return;  
+    }
+    if(pics.type==="image/jpeg" || pics.type==="image/png"){
+      const data=new FormData();
+      data.append("file",pics);
+      data.append("upload_preset","chat-shala");
 
+      data.append("cloud_name","leetcode");
+      fetch("https://api.cloudinary.com/v1_1/leetcode/image/upload",{
+        method:'post',
+        body: data,
+      }).then((res)=>res.json())
+      .then((data)=>{
+        // console.log(data);
+        setPic(data.url.toString());
+        setLoading(false);
+      }).catch((err)=>{
+        console.log(err);setLoading(false);
+      });
+    }else{
+      toast({
+        title: 'Please Select an Image!',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position:"bottom",
+      });  
+      setLoading(false);
+      return;
+    }
   }
-  const submitHandler=()=>{
+  const submitHandler=async()=>{
+    setLoading(true);
+    if(!name || !email || !password || !confirmpassword){
+      toast({
+        title: 'Please Fill all the Fields',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position:"bottom",
+      });  
+      setLoading(false);
+      return;
+    }
+    if(password!==confirmpassword){
+      toast({
+        title: 'Please Select an Image!',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position:"bottom",
+      });  
+      setLoading(false);
+      return;
+    }
 
+    try {
+      const config={
+        headers:{
+          "Content-type":"application/json",
+        },
+      };
+      const {data}= await axios.post("/api/user",{name,email,password},
+      config);
+      toast({
+        title: 'Registration Successfull',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position:"bottom",
+      });  
+      localStorage.setItem('userInfo',JSON.stringify(data));
+      setLoading(false);
+      navigate('/chats');
+    } catch (error) {
+      toast({
+        title: 'Error occured',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position:"bottom",
+      });  
+      setLoading(false);
+    }
   }
   return (
   <VStack spacing='5px' color="black">
@@ -60,7 +155,7 @@ const Signup = () => {
       <FormLabel>Upload Your Pic</FormLabel>
         <Input type="file"p={1.5} accept="image/*" color="black" placeholder='Enter your Pic' onChange={(e)=>{postDetails(e.target.files[0])}}/>
     </FormControl>
-    <Button colorScheme={"blue"} width="100%" style={{marginTop:15}} onClick={submitHandler}>
+    <Button colorScheme={"blue"} width="100%" style={{marginTop:15}} onClick={submitHandler}isLoading={loading}>
               Sign Up
     </Button>
   </VStack>
