37b902bfe872752054f56173a818c28bde628716
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-03-29 19:38:05 +0530

    createv remove chats members added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const { chats } = require('./data/data');
 const dotenv = require('dotenv');
 const connectDB = require('./Config/db');
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 //     res.send(singleChat);
 // })
 app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
